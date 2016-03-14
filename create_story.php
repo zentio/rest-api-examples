fae467f660878a985fc9216912e52556b93d4623
kablex <oscar.kablex@gmail.com>
2016-03-14 10:25:55 +0100

    Added file upload to REST API example

--- create_story.php
+++ create_story.php
@@ -4,18 +4,27 @@
 /* @author antonio.moreno@zent.io */
 
 $zentio_instance = 'example';
-$api_key = "x4W4Fb6saceBoMuvK7l0grzVEyr1W6";
-$url = "https://" . $zentio_instance . ".zent.io/api/v1/story/new?apikey=" . $api_key; 
+$api_key = "ckgGw9PRZGeEe9eh_avH2I3IQJQQeS";
+$url = "https://" . $zentio_instance . ".zent.io/api/v1/story/new?apikey=" . $api_key;
 
 $extendedModel = '{"Custom Field": "This is an example"}';
 
-$fields = array('customer' => urlencode('john.doe@email.com'),
-	           'user' => null,
+//encode url-safe base64 for files
+$path = 'file.type';
+$type = pathinfo($path, PATHINFO_EXTENSION);
+$data = file_get_contents($path);
+$base64 = rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+$attachments = '{"file.type": "' . $base64 . '"}';
+
+$fields = array('customer' => urlencode('benjamin@gmail.com'),
+	           'user' => urlencode('benjamin@gmail.com'),
 	           'subject' => urlencode("My order is broken"),
-	           'message' => urlencode("This is my message, you can answer to my e-mail account."),
-	           'extendedModel' => $extendedModel);
+	           'message' => urlencode("This is my message, you can answer to my e-mail accountsss."),
+						 'state'	=> urlencode("open"),
+	           'extendedModel' => $extendedModel,
+					 	 'attachments' => $attachments);
 
-
+$fields_string = '';
 //url-ify the data for the POST
 foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 rtrim($fields_string, '&');
